d6e8d5096428e4d46014d88354f580c7143b1992
Zach Stephan <zas5035@gmail.com>
2025-02-10 17:55:26 -0500

    Add project files.

---
+++ Sp2025 CSC 221 Workflow Project.cpp
@@ -0,0 +1,21 @@
+// Sp2025 CSC 221 Workflow Project.cpp : Problem 2.14
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string first;
+    string middle;
+    string last;
+
+    first = "Z";
+    middle = "A";
+    last = "S";
+
+
+    cout << first << endl << middle << endl << last << endl;
+    return 0;
+}
